52262ec8d7cee8ca7744ba7aa05166fc5c50d5dc
Matt Keeley <keeley55@me.com>
2023-11-17 11:10:13 -0700

    updating security rules

--- waf/security_rules.py
+++ waf/security_rules.py
@@ -1,16 +1,16 @@
 # waf/security_rules.py
 from flask import request
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def xss_rule(request):
-    if (
+    return (
         "<script>" in request.user_agent.string
         or "<script>" in request.path
-        or "<script>" in request.query_string.decode("utf-8")
-    ):
-        return True
-    return False
-
+        or "%3Cscript%3E" in request.query_string.decode("utf-8")
+        or any("<script>" in value for value in request.headers.values())
+        or (request.method == 'POST' and any("<script>" in value for value in request.form.values()))
+    )
 
 def sql_injection_rule(request):
     sql_keywords = [
@@ -30,6 +30,6 @@ def sql_injection_rule(request):
 
 
 def path_traversal_rule(request):
-    if "../" in request.path or "/../" in request.path:
+    if "../" in request.path or "/../" in request.path or"../" in request.query_string.decode("utf-8") or "/../" in request.query_string.decode("utf-8"):
         return True
     return False
